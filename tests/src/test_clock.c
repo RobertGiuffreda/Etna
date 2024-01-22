34c5be1fcf341a5d5b70655bee0f78ea8702102f
RobertGiuffreda <55211632+RobertGiuffreda@users.noreply.github.com>
2024-01-21 19:54:58 -0500

    Delete tests/src directory

--- tests/src/test_clock.c
+++
@@ -1,9 +0,0 @@
-#include "test_clock.h"
-
-void test_clock_start(test_clock* clk) {
-    clk->start = clock();
-}
-
-void test_clock_update(test_clock* clk) {
-    clk->end = clock();
-}
--- tests/src/test_manager_tests.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <defines.h>
-#include <core/logger.h>
-
-void test_manager_tests_register_all_tests(void);
-
-b8 test_manager_tests_test(void);
